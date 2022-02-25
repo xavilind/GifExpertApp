67dc8d073aebed76c1bfcb1db41631fe8f92035e
xavilind <xavilind7@gmail.com>
2022-02-25 14:58:11 -0600

    TitulosImagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
+
 
 export const GifGrid = ({category}) => {
 
 
-    const [count, setCount] = useState(0)
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
@@ -24,14 +26,22 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs);
-
+        setImages( gifs );
 
     }
          return (
     <div>
         <h3>{ category }</h3>
-        <h3> {count} </h3>
-        <button onClick={ () => setCount( count + 1 ) }></button>
+        
+        {
+            images.map( img => (
+                <GifGridItem 
+                    key = { img.id }
+                    {...img } 
+
+                />
+            ))
+        }
     </div>
   )
 }
