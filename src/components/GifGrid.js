8602bffb91874619e8bd2cdaef71e1ea576ba895
xavilind <xavilind7@gmail.com>
2022-02-25 16:11:07 -0600

    Helpers-getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 
 export const GifGrid = ({category}) => {
@@ -8,27 +9,10 @@ export const GifGrid = ({category}) => {
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, [])
-
-    const getGifs = async () => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=w2gQILuCPAiMncgnww7l3NxbjUb2x7Yk';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages( gifs );
-
-    }
+       
+getGifs( category )
+        .then(setImages);
+}, [ category ])
          return (
             <>
             <h3>{ category }</h3>
